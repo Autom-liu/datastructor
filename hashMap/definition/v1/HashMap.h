f71ff09eb9e8758780e87c50bbf00db6753fdf7a
Autom-liu <349576362@163.com>
2020-10-11 00:18:18 +0800

    基于哈希表的映射

---
+++ hashMap/definition/v1/HashMap.h
@@ -0,0 +1,212 @@
+#ifndef HASHMAP_H_INCLUDED
+#define HASHMAP_H_INCLUDED
+
+template <typename T>
+class HashedCompare {
+public:
+    virtual unsigned int hashCode(T obj) = 0;
+};
+
+class IntergerHashed : public HashedCompare<int> {
+public:
+    unsigned int hashCode(int obj) {
+        return obj;
+    }
+};
+
+class StringHashed : public HashedCompare<string> {
+public:
+    unsigned int hashCode(string s) {
+        unsigned int h = 0;
+        for(unsigned int i = 0; i < s.length(); i++) {
+            h = 31 * h + s.at(i);   /// 31作为不大不小的质数，作为进制位（31进制）
+        }
+        return h;
+    }
+};
+
+template <typename K, typename V>
+class LinkNode {
+ public:
+     K key;
+     V value;
+     LinkNode<K, V>* next;
+     LinkNode(K key, V value) {
+         this->key = key;
+         this->value = value;
+         this->next = NULL;
+     }
+};
+
+template <typename K, typename V>
+class Entry {
+ public:
+     K key;
+     V value;
+     Entry(K key, V value) {
+         this->key = key;
+         this->value = value;
+     }
+};
+
+template <typename K, typename V>
+class HashMap {
+private:
+    LinkNode<K, V>** table;
+    int size;
+    int capacity;
+    double loadFactor;
+    HashedCompare<K>* hashedCompare;
+
+    int hash(K key) {
+        return hashedCompare->hashCode(key) % capacity;
+    }
+
+    void putNode(LinkNode<K, V>** table, LinkNode<K, V>* node) {
+        int h = hash(node->key);
+        LinkNode<K, V>* current = table[h];
+        if(current == NULL) {
+            table[h] = node;
+        } else {
+            while(current->next != NULL) {
+                current = current->next;
+            }
+            current->next = node;
+        }
+    }
+
+    void resize(int newSize) {
+        int oldCapacity = this->capacity;
+        LinkNode<K, V>** newTable = new LinkNode<K, V>*[newSize];
+        this->capacity = newSize;
+        int index = 0;
+        while(index < oldCapacity) {
+            LinkNode<K, V>* node = table[index];
+            if(node != NULL) {
+                node = node->next;
+            }
+            while(node == NULL && index < oldCapacity) {
+                node = table[index++];
+            }
+            if(node != NULL) {
+                putNode(newTable, node);
+            }
+        }
+        delete this->table;
+        this->table = newTable;
+
+    }
+
+public:
+    HashMap(HashedCompare<K>* hashCompare, int capacity = 31, double loadFactor = 0.75f) {
+        this->table = new LinkNode<K, V>*[capacity];
+        this->size = 0;
+        this->capacity = capacity;
+        this->loadFactor = loadFactor;
+        this->hashedCompare = hashCompare;
+        for(int i = 0; i < capacity; i++) {
+            table[i] = NULL;
+        }
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    void put(K key, V value) {
+        putNode(table, new LinkNode<K, V>(key, value));
+        size++;
+    }
+
+    V get(K key) {
+        int h = hash(key);
+        LinkNode<K, V>* current = table[h];
+        while(current != NULL) {
+            if(current->key == key) {
+                return current->value;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+
+    bool contains(K key) {
+        int h = hash(key);
+        LinkNode<K, V>* current = table[h];
+        while(current != NULL) {
+            if(current->key == key) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+
+    V remove(K key) {
+        int h = hash(key);
+        LinkNode<K, V>* current = table[h];
+        LinkNode<K, V>** prev = &current;
+        while(current != NULL) {
+            if(current->key == key) {
+                *prev = current->next;
+                size--;
+                return current->value;
+            }
+            prev = &(current->next);
+            current = current->next;
+        }
+        return false;
+    }
+
+    class iterator;
+
+    HashMap<K, V>::iterator* newIterator() {
+        return new HashMap<K, V>::iterator(capacity, 0, NULL, table);
+    }
+
+    class iterator {
+    private:
+        int capacity;
+        int index;
+        LinkNode<K, V>* node;
+        LinkNode<K, V>** table;
+    public:
+        iterator(int capacity, int index, LinkNode<K, V>* node, LinkNode<K, V>** table) {
+            this->capacity = capacity;
+            this->index = index;
+            this->node = node;
+            this->table = table;
+        }
+        bool hasNext() {
+            return index < capacity;
+        }
+        Entry<K, V> next() {
+            if(node != NULL) {
+                node = node->next;
+            }
+            while(node == NULL) {
+                node = table[index++];
+            }
+            return Entry<K, V>(node->key, node->value);
+        }
+    };
+
+    ~HashMap() {
+        for(int i = 0; i < capacity; i++) {
+            LinkNode<K, V>*& current = table[i];
+            while(current != NULL) {
+                LinkNode<K, V>* ele = current;
+                current = current->next;
+                delete ele;
+            }
+        }
+    }
+
+};
+
+
+#endif // HASHMAP_H_INCLUDED
---
+++ hashMap/definition/v1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+#include "HashMap.h"
+
+template<typename K, typename V>
+void printMap(HashMap<K, V>* hmap) {
+    typename HashMap<K, V>::iterator* it = hmap->newIterator();
+    while(it->hasNext()) {
+        Entry<K, V> entry = it->next();
+        cout<<"key = "<<entry.key<<" value = "<<entry.value<<endl;
+    }
+    delete it;
+}
+
+int main()
+{
+    HashMap<string, int>* hmap = new HashMap<string, int>(new StringHashed());
+    for(int i = 0; i < 30; i++) {
+        int x = rand() % 100;
+        char c[3];
+        string ss(c);
+        sprintf(c, "t%d", x);
+        hmap->put(c, x * 10);
+    }
+    printMap(hmap);
+    delete hmap;
+    return 0;
+}
